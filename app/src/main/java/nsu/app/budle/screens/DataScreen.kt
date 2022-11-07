5ac8dfaee69398f63d63fdbb8e24678bfebda0e9
NikFive <NikIYPT@yandex.ru>
2022-11-07 23:40:39 +0700

    develop/add navigation and first screen

---
+++ app/src/main/java/nsu/app/budle/screens/DataScreen.kt
@@ -0,0 +1,9 @@
+package nsu.app.budle.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+@Composable
+fun DataScreen(navController: NavHostController) {
+
+}
---
+++ app/src/main/java/nsu/app/budle/ui/theme/Type.kt
@@ -0,0 +1,34 @@
+package nsu.app.budle.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.budle.R
+
+val fonts = FontFamily(
+    Font(R.font.gilroy_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.gilroy_semibolditalic, weight = FontWeight.SemiBold, style = FontStyle.Italic)
+)
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.SemiBold,
+        fontStyle = FontStyle.Italic
+    ),
+    labelSmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.SemiBold,
+    )
+)
