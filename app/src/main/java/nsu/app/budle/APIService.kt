2affc3a97633e28310d9ea15921b1b72f9de30e8
NikFive <NikIYPT@yandex.ru>
2022-12-01 19:23:26 +0700

    develop/add register POST request to the APIService.kt

--- app/src/main/java/nsu/app/budle/APIService.kt
+++ app/src/main/java/nsu/app/budle/APIService.kt
@@ -17,6 +17,9 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface APIService {
+    @POST("register")
+    suspend fun sendUserRequest(@Body requestBody: RequestBody): Response<ResponseBody>
+
     @POST("checkCode")
     suspend fun checkCodeRequest(@Body requestBody: RequestBody): Response<ResponseBody>
 
@@ -60,6 +63,31 @@ suspend fun checkCode(number: String, code: String): Answer? {
     var result: Answer? = null
     val message: Answer? = CoroutineScope(Dispatchers.IO).async {
         val response = service.checkCodeRequest(requestBody)
+        withContext(Dispatchers.IO) {
+            if (response.isSuccessful) {
+                val jsonData = Json.parseToJsonElement(response.body()!!.string())
+                result = Json.decodeFromJsonElement(jsonData)
+            } else {
+                Log.e("RETROFIT_ERROR", response.code().toString())
+            }
+        }
+        return@async result
+    }.await()
+    return message
+}
+
+suspend fun sendUser(number: String, name: String, password: String): Answer? {
+    val retrofit = Retrofit.Builder().baseUrl("https://budle-app.herokuapp.com/").build()
+    val service = retrofit.create(APIService::class.java)
+    val jsonObject = JSONObject()
+    jsonObject.put("name", name)
+    jsonObject.put("phoneNumber", number)
+    jsonObject.put("password", password)
+    val jsonObjectString = jsonObject.toString()
+    val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
+    var result: Answer? = null
+    val message: Answer? = CoroutineScope(Dispatchers.IO).async {
+        val response = service.sendUserRequest(requestBody)
         withContext(Dispatchers.IO) {
             if (response.isSuccessful) {
                 val jsonData = Json.parseToJsonElement(response.body()!!.string())
