5380163e46efdd1f50bf330cb7956de455b1f044
Fagor <oleg-veber@mail.ru>
2023-03-24 12:19:44 +0700

    change budleNavHost

--- app/src/main/java/fit/budle/navigation/BudleNavHost.kt
+++ app/src/main/java/fit/budle/navigation/BudleNavHost.kt
@@ -36,7 +36,7 @@ fun BudleNavHost() {
         composable(NavRoute.UserProfile.route) { UserProfileScreen(navController = navController) }
         composable(NavRoute.UserProfileSettings.route) { UserProfileSettingsScreen(navController = navController) }
         composable(NavRoute.UserProfileFavorites.route) { UserProfileFavoritesScreen(navController = navController) }
-        composable(NavRoute.UserProfileBookings.route) { UserProfileBookingsScreen(navController = navController) }
+       // composable(NavRoute.UserProfileBookings.route) { UserProfileBookingsScreen(navController = navController) }
         composable(NavRoute.UserProfileReviews.route) { UserProfileReviewsScreen(navController = navController) }
     }
 }
