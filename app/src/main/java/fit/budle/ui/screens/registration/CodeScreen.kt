4d2d648d6251f711a25e7e9c7695f8a218cdf084
Fagor <oleg-veber@mail.ru>
2023-05-27 00:34:51 +0700

    fix code screen

--- app/src/main/java/fit/budle/ui/screens/registration/CodeScreen.kt
+++ app/src/main/java/fit/budle/ui/screens/registration/CodeScreen.kt
@@ -13,11 +13,9 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.key.*
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -32,14 +30,14 @@ import fit.budle.ui.theme.fillPurple
 import fit.budle.ui.theme.textGray
 import fit.budle.viewmodel.customer.RegistrationViewModel
 
-@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun CodeScreen(
     navHostController: NavHostController,
     viewModel: RegistrationViewModel,
 ) {
-
     val focusManager = LocalFocusManager.current
+    val firstInput = 0
+    val lastInput = 3
     val errorState = remember { mutableStateOf(false) }
 
     Surface(
@@ -98,60 +96,39 @@ fun CodeScreen(
                     itemsIndexed(viewModel.states) { i, _ ->
                         val inputColor = if (!errorState.value || viewModel.states[i].isNotEmpty())
                             backgroundLightBlue else backgroundError
-                        // start from first input
-                        if (i == 0 || viewModel.states[i - 1] != "") {
-                            TextField(
-                                value = viewModel.states[i],
-                                modifier = Modifier
-                                    .padding(top = 16.dp, start = 8.dp, end = 8.dp)
-                                    .width(60.dp)
-                                    // change focus when backspace
-                                    .onKeyEvent { event: KeyEvent ->
-                                        if (event.type == KeyEventType.KeyUp &&
-                                            event.key == Key.Backspace &&
-                                            viewModel.states[i].isEmpty()
-                                        ) {
-                                            focusManager.moveFocus(FocusDirection.Left)
-                                            true
-                                        } else false
-                                    },
-                                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                                onValueChange = {
-                                    if (it.length <= 1) {
-                                        if (i == 0 || viewModel.states[i - 1] != "") {
-                                            if (errorState.value) errorState.value = false
-                                            viewModel.states[i] = it
-                                            if (viewModel.states[i] != "") {
-                                                focusManager.moveFocus(FocusDirection.Right)
-                                            } else focusManager.moveFocus(FocusDirection.Left)
-                                        }
-                                    } else focusManager.moveFocus(FocusDirection.Right)
-                                },
-                                colors = TextFieldDefaults.textFieldColors(
-                                    focusedIndicatorColor = fillPurple,
-                                    unfocusedIndicatorColor = inputColor,
-                                    disabledIndicatorColor = Color.Transparent,
-                                    containerColor = Color.Transparent
-                                ),
-                                textStyle = MaterialTheme.typography.displayLarge
-                            )
-                        } else if (viewModel.states[i - 1] == "") {
-                            TextField(
-                                value = "",
-                                modifier = Modifier
-                                    .padding(top = 16.dp, start = 8.dp, end = 8.dp)
-                                    .width(60.dp),
-                                onValueChange = {},
-                                colors = TextFieldDefaults.textFieldColors(
-                                    focusedIndicatorColor = inputColor,
-                                    unfocusedIndicatorColor = inputColor,
-                                    disabledIndicatorColor = Color.Transparent,
-                                    containerColor = Color.Transparent,
-                                ),
-                                textStyle = MaterialTheme.typography.displayLarge,
-                                readOnly = true
-                            )
-                        }
+                        TextField(
+                            value = viewModel.states[i],
+                            modifier = Modifier
+                                .padding(top = 16.dp, start = 8.dp, end = 8.dp)
+                                .width(60.dp),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            onValueChange = {
+                                if (it.length <= 1) {
+                                    viewModel.states[i] = it
+                                }
+                                if (it.isNotEmpty()) {
+                                    if (i == lastInput) {
+                                        focusManager.clearFocus()
+                                    } else {
+                                        focusManager.moveFocus(FocusDirection.Next)
+                                    }
+                                } else {
+                                    if (i == firstInput) {
+                                        focusManager.clearFocus()
+                                    } else {
+                                        focusManager.moveFocus(FocusDirection.Previous)
+                                    }
+                                }
+                            },
+                            colors = TextFieldDefaults.colors(
+                                focusedIndicatorColor = fillPurple,
+                                unfocusedIndicatorColor = inputColor,
+                                disabledIndicatorColor = Color.Transparent,
+                                unfocusedContainerColor = Color.Transparent,
+                                focusedContainerColor = Color.Transparent
+                            ),
+                            textStyle = MaterialTheme.typography.displayLarge
+                        )
                     }
                 }
                 if (viewModel.requestException.isNotEmpty()) {
@@ -159,7 +136,7 @@ fun CodeScreen(
                         text = viewModel.requestException,
                         style = MaterialTheme.typography.bodyMedium,
                         color = backgroundError,
-                        modifier = Modifier.padding(top = 10.dp)
+                        modifier = Modifier.padding(top = 10.dp, start = 15.dp)
                     )
                 }
             }
@@ -187,7 +164,9 @@ fun CodeScreen(
                         Handler(Looper.getMainLooper()).postDelayed({
                             if (viewModel.requestException.isEmpty()) {
                                 navHostController.navigate("dataScreen")
-                            } else viewModel.states = mutableStateListOf("", "", "", "")
+                            } else {
+                                viewModel.states = mutableStateListOf("", "", "", "")
+                            }
                         }, 1000)
                     }
                 },
