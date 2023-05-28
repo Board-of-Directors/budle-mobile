d5da2f829a827e59657efefa5f096009c1dd9857
Fagor <oleg-veber@mail.ru>
2023-05-28 17:08:58 +0700

    change input length in est creation

--- app/src/main/java/fit/budle/ui/components/atoms/inputs/text_inputs/BudleSingleLineInput.kt
+++ app/src/main/java/fit/budle/ui/components/atoms/inputs/text_inputs/BudleSingleLineInput.kt
@@ -1,8 +1,17 @@
 package fit.budle.ui.components.atoms.inputs.text_inputs
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -54,7 +63,8 @@ fun BudleSingleLineInput(
             onValueChange = { textInputState = it },
             error = isError,
             leadingIcon = leadingIcon,
-            trailingIcon = trailingIcon
+            trailingIcon = trailingIcon,
+            inputLength = 30
         )
         onValueChange(textInputState)
         if (isError) {
